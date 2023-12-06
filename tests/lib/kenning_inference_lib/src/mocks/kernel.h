9183f79b9844ef4259a2c0da8bd7d37ffbef0fc8
Maciej Torhan <mtorhan@antmicro.com>
2023-12-06 10:22:36 +0100

    [#51500] tests: lib: kenning_inference_lib: src: mocks: kernel: Add kernel mocks
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

---
+++ tests/lib/kenning_inference_lib/src/mocks/kernel.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2023 Antmicro <www.antmicro.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef KENNING_INFERENCE_LIB_TESTS_MOCKS_KERNEL_H_
+#define KENNING_INFERENCE_LIB_TESTS_MOCKS_KERNEL_H_
+
+#include <errno.h>
+
+#define K_TICKS(x) x
+
+#endif // KENNING_INFERENCE_LIB_TESTS_MOCKS_KERNEL_H_
