3a7e303ca65a6f3773de1bf5242b50d1d36adf47
Maciej Torhan <mtorhan@antmicro.com>
2024-01-15 15:02:22 +0100

    [#51536] tests: kenning_inference_lib: mocks: uart: Fix include guard name
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- tests/lib/kenning_inference_lib/src/mocks/uart.h
+++ tests/lib/kenning_inference_lib/src/mocks/uart.h
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef KENNING_INFERENCE_LIB_TESTS_MOCKS_UART_H_
-#define KENNING_INFERENCE_LIB_TESTS_MOCKS_UART_H_
+#ifndef TESTS_KENNING_INFERENCE_LIB_MOCKS_UART_H_
+#define TESTS_KENNING_INFERENCE_LIB_MOCKS_UART_H_
 
 #define DT_NODELABEL(node) node
 #define DEVICE_DT_GET(node) NULL
@@ -21,4 +21,4 @@ int uart_poll_in(const struct device *dev, unsigned char *p_char);
 
 void uart_poll_out(const struct device *dev, unsigned char out_char);
 
-#endif // KENNING_INFERENCE_LIB_TESTS_MOCKS_UART_H_
+#endif // TESTS_KENNING_INFERENCE_LIB_TESTS_MOCKS_UART_H_
