c534ebcf42b198e87794985268ce496f39d6cd1f
Maciej Torhan <mtorhan@antmicro.com>
2024-01-15 15:02:22 +0100

    [#51536] tests: kenning_inference_lib: mocks: log: Fix include guard name
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- tests/lib/kenning_inference_lib/src/mocks/log.h
+++ tests/lib/kenning_inference_lib/src/mocks/log.h
@@ -4,6 +4,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#ifndef TESTS_KENNING_INFERENCE_LIB_MOCKS_LOG_H_
+#define TESTS_KENNING_INFERENCE_LIB_MOCKS_LOG_H_
+
 #include <stdio.h>
 
 #define LOG_MODULE_REGISTER(...)
@@ -12,3 +15,5 @@
 #define LOG_DBG(fmt, ...) printf("[DBG] " fmt, ##__VA_ARGS__);
 #define LOG_INF(fmt, ...) printf("[INF] " fmt, ##__VA_ARGS__);
 #define LOG_WRN(fmt, ...) printf("[WRN] " fmt, ##__VA_ARGS__);
+
+#endif // TESTS_KENNING_INFERENCE_LIB_MOCKS_LOG_H_
