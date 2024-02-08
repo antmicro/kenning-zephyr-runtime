38066fc2fbd21743c6b2b5cd0870f2330d9f356c
Maciej Torhan <mtorhan@antmicro.com>
2024-02-08 16:35:56 +0100

    [#54920] tests: demo: Add demo tests
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

---
+++ tests/demo/test_demo.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2023-2024 Antmicro <www.antmicro.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from pathlib import Path
+
+from pyrenode3.wrappers import Emulation, TerminalTester
+import pytest
+
+runtime_binaries = [binary for binary in Path("./artifacts/").glob("*.elf")]
+
+
+class TestDemo:
+    @pytest.mark.parametrize(
+        "binary", runtime_binaries, ids=[b.stem for b in runtime_binaries]
+    )
+    def test_demo(self, binary: Path):
+        *_, board = binary.stem.split("-")
+        repl_path = Path(f"artifacts/{board}.repl")
+
+        if not repl_path.exists():
+            pytest.xfail(f"Missing repl for {board}")
+
+        emulation = Emulation()
+
+        platform = emulation.add_mach(board)
+        platform.load_repl(repl_path.resolve())
+        platform.load_elf(binary)
+
+        testers = [
+            TerminalTester(getattr(platform.sysbus, uart_name), 10)
+            for uart_name in dir(platform.sysbus)
+            if "uart" in uart_name or "usart" in uart_name
+        ]
+
+        emulation.StartAll()
+
+        assert any(
+            tester.WaitFor("I: inference done", includeUnfinishedLine=True)
+            for tester in testers
+        ), "\n\n".join(tester.GetReport() for tester in testers)
+
+        emulation.clear()
