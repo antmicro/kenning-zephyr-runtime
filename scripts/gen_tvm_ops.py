9c7ea3a7eb17dfd9d13da55275cebdc56d123866
Maciej Torhan <mtorhan@antmicro.com>
2024-02-19 15:30:28 +0100

    [#55307] scripts: gen_tvm_ops: Add scripts for generating TVM ops source and header
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

---
+++ scripts/gen_tvm_ops.py
@@ -0,0 +1,59 @@
+# Copyright (c) 2023-2024 Antmicro <www.antmicro.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Python script for generating TVM ops for given model.
+"""
+
+import argparse
+import re
+from pathlib import Path
+
+from kenning.optimizers.tvm import TVMCompiler
+from kenning.utils.resource_manager import ResourceURI
+from kenning.utils.logger import KLogger
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(__doc__)
+
+    parser.add_argument(
+        "--input-path",
+        "-i",
+        type=ResourceURI,
+        help="Path to input model",
+        required=True,
+    )
+    parser.add_argument(
+        "--output-path", "-o", type=Path, help="Path to compiled model", required=True
+    )
+    parser.add_argument(
+        "--template-header-path",
+        "-t",
+        type=ResourceURI,
+        help="Path to header template",
+        required=True,
+    )
+    parser.add_argument("--board", "-b", type=str, help="Target board", required=True)
+    parser.add_argument(
+        "--verbosity", "-v", type=str, help="Logging verbosity", default="INFO"
+    )
+
+    args = parser.parse_args()
+    KLogger.set_verbosity(level=args.verbosity)
+
+    if not args.input_path.exists():
+        raise FileNotFoundError(f"{args.input_path} does not exist")
+
+    compiler = TVMCompiler(
+        dataset=None,
+        compiled_model_path=args.output_path,
+        model_framework="any",
+        target="zephyr",
+        target_microtvm_board=args.board,
+        zephyr_template_header=args.template_header_path,
+    )
+
+    compiler.compile(args.input_path)
+
+    KLogger.info(f"TVM ops saved to {args.output_path}")
