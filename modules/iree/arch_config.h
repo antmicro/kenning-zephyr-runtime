5a9d32816bb415a8969f507911c8763b4abc3d7f
Grzegorz Latosinski <glatosinski@antmicro.com>
2024-04-30 14:01:14 +0200

    [#58000] modules: iree: arch_config: Removed currently generated header
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- modules/iree/arch_config.h
+++
@@ -1,8 +0,0 @@
-#if defined(IREE_ARCH_ARM_32) || defined(IREE_ARCH_RISCV_32) || defined(IREE_ARCH_X86_32)
-#define IREE_UK_POINTER_SIZE 4
-#elif defined(IREE_ARCH_ARM_64) || defined(IREE_ARCH_RISCV_64) || defined(IREE_ARCH_X86_64)
-#define IREE_UK_POINTER_SIZE 8
-#endif
-#ifdef IREE_ARCH_ARM_64
-#define IREE_UK_ARCH_ARM_64
-#endif // IREE_ARCH_ARM_64
