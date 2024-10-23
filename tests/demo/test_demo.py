354863573ed1ed1bd500b5c5d5299259588f4b5f
Maciej Torhan <mtorhan@antmicro.com>
2024-10-23 11:33:51 +0200

    [#57722] tests: demo: test_demo: Add checking predictions
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- tests/demo/test_demo.py
+++ tests/demo/test_demo.py
@@ -2,17 +2,37 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+import re
 from pathlib import Path
 
-from pyrenode3.wrappers import Emulation, TerminalTester
+import numpy as np
 import pytest
+from pyrenode3.wrappers import Emulation, TerminalTester
 
-runtime_binaries = [binary for binary in Path("./artifacts/").glob("*.elf")]
+RUNTIME_BINARIES = [binary for binary in Path("./artifacts/").glob("*.elf")]
+FLOAT_RE = r"(?:-?[0-9]+.[0-9]+)|(?:nan)"
+MODEL_OUTPUT_RE = re.compile(
+    rf"I: model output: \[wing: ({FLOAT_RE}), ring: ({FLOAT_RE}), "
+    rf"slope: ({FLOAT_RE}), negative: ({FLOAT_RE})\]"
+)
+EXPECTED_PREDICTIONS = [0, 3, 2, 0, 1, 2, 0, 0, 0, 2]
+
+
+@pytest.fixture(autouse=True)
+def test_setup_cleanup():
+    # setup
+    pass
+
+    # run test
+    yield
+
+    # cleanup
+    Emulation().clear()
 
 
 class TestDemo:
     @pytest.mark.parametrize(
-        "binary", runtime_binaries, ids=[b.stem for b in runtime_binaries]
+        "binary", RUNTIME_BINARIES, ids=[b.stem for b in RUNTIME_BINARIES]
     )
     def test_demo(self, binary: Path):
         *_, board = binary.stem.split("-")
@@ -29,7 +49,7 @@ class TestDemo:
             platform = emulation.get_mach(board)
 
         platform.load_repl(repl_path.resolve())
-        platform.load_elf(binary)
+        platform.load_elf(binary.resolve())
 
         testers = [
             TerminalTester(getattr(platform.sysbus, uart_name), 30)
@@ -51,4 +71,20 @@ class TestDemo:
             for tester in testers
         ), "\n\n".join(tester.GetReport() for tester in testers)
 
-        emulation.clear()
+        runtime_log = None
+        for tester in testers:
+            if "Booting Zephyr OS" in tester.GetReport():
+                runtime_log = tester.GetReport()
+                break
+
+        predictions = re.findall(MODEL_OUTPUT_RE, runtime_log)
+
+        assert len(EXPECTED_PREDICTIONS) == len(
+            predictions
+        ), f"invalid predictions:\n{predictions}"
+
+        predictions = np.array(predictions, dtype=np.float32).argmax(axis=1)
+
+        assert (
+            EXPECTED_PREDICTIONS == predictions.tolist()
+        ), f"Invalid predictions:\n{predictions}"
