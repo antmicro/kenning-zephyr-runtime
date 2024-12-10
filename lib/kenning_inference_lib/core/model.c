9a5af381928a08448b25561cba916b09b73ab261
Anna Roszkiewicz <aroszkiewicz@internships.antmicro.com>
2024-12-10 17:09:24 +0100

    [#69358] lib: kenning_inference_lib: core: model: Pointer error handling

--- lib/kenning_inference_lib/core/model.c
+++ lib/kenning_inference_lib/core/model.c
@@ -203,9 +203,11 @@ status_t model_load_weights(const uint8_t *model_weights_data, const size_t data
     status_t status = STATUS_OK;
     struct msg_loader *msg_loader_model = g_ldr_tables[1][LOADER_TYPE_MODEL];
 
+    RETURN_ERROR_IF_POINTER_INVALID(model_weights_data, MODEL_STATUS_INV_PTR);
+    RETURN_ERROR_IF_POINTER_INVALID(msg_loader_model, MODEL_STATUS_INV_PTR);
+
     msg_loader_model->reset(msg_loader_model, 0);
     status = msg_loader_model->save(msg_loader_model, (uint8_t *)model_weights_data, data_size);
-    RETURN_ON_ERROR_LOG(status, status, "Model loader failed: %d", status);
 
     return model_load_weights_from_loader();
 }
@@ -215,9 +217,11 @@ status_t model_load_struct(const uint8_t *model_struct_data, const size_t data_s
     status_t status = STATUS_OK;
     struct msg_loader *msg_loader_iospec = g_ldr_tables[0][LOADER_TYPE_IOSPEC];
 
+    RETURN_ERROR_IF_POINTER_INVALID(model_struct_data, MODEL_STATUS_INV_PTR);
+    RETURN_ERROR_IF_POINTER_INVALID(msg_loader_iospec, MODEL_STATUS_INV_PTR);
+
     msg_loader_iospec->reset(msg_loader_iospec, 0);
     status = msg_loader_iospec->save(msg_loader_iospec, model_struct_data, data_size);
-    RETURN_ON_ERROR_LOG(status, status, "iospec loader failed: %d", status);
 
     return model_load_struct_from_loader();
 }
@@ -227,9 +231,11 @@ status_t model_load_input(const uint8_t *model_input, const size_t model_input_s
     status_t status = STATUS_OK;
     struct msg_loader *msg_loader_data = g_ldr_tables[1][LOADER_TYPE_DATA];
 
+    RETURN_ERROR_IF_POINTER_INVALID(model_input, MODEL_STATUS_INV_PTR);
+    RETURN_ERROR_IF_POINTER_INVALID(msg_loader_data, MODEL_STATUS_INV_PTR);
+
     msg_loader_data->reset(msg_loader_data, 0);
     status = msg_loader_data->save(msg_loader_data, model_input, model_input_size);
-    RETURN_ON_ERROR_LOG(status, status, "Data loader failed: %d", status);
 
     return model_load_input_from_loader();
 }
