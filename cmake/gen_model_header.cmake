f9be7f023095c4875048f8f022d6641f7cd52daa
Maciej Torhan <mtorhan@antmicro.com>
2025-02-27 14:30:21 +0100

    [#73576] cmake: gen_model_header: Improve docstrings
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- cmake/gen_model_header.cmake
+++ cmake/gen_model_header.cmake
@@ -2,7 +2,10 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-# When model is built, its header will be saved to provided path,
+# Saves model data header to a specified path. It requires
+# CONFIG_KENNING_MODEL_PATH to be specified.
+#
+# @param output_path Path to which model data header should be saved.
 function(kenning_gen_model_header output_path)
   set(model_data_path "${KENNING_MODULE_BINARY_DIR}/lib/kenning_inference_lib/model_data.h")
 
