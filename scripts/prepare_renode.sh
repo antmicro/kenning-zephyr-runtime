95df0594c0b83c9c6b6bf6e8f692fa4d6e0903cb
Karl Roldan <kroldan@internships.antmicro.com>
2026-03-03 10:50:28 +0100

    [#92500] scripts: prepare_renode.sh: Updated Renode directory naming convention

--- scripts/prepare_renode.sh
+++ scripts/prepare_renode.sh
@@ -8,10 +8,10 @@ if [ -d renode_latest ] ; then
   echo "Renode already downloaded."
 else
   echo "Downloading Renode..."
-  curl https://builds.renode.io/renode-latest.linux-portable-dotnet.tar.gz -o renode-latest.tar.gz
+  curl https://builds.renode.io/renode-latest.linux-portable.tar.gz -o renode-latest.tar.gz
   tar -xf renode-latest.tar.gz
   rm -f renode-latest.tar.gz
-  mv -f renode_*-dotnet_portable renode_latest
+  mv -f renode_*-portable renode_latest
 fi
 
 echo "Preparing environment..."
