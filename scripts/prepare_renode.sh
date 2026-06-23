402b08e48b3e34e24d56335f13e3ef982d31461b
Grzegorz Latosinski <glatosinski@antmicro.com>
2026-06-23 12:29:13 +0200

    [#97882] scripts: prepare_renode.sh: Added configuring Renode version
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- scripts/prepare_renode.sh
+++ scripts/prepare_renode.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
-# Copyright (c) 2025 Antmicro <www.antmicro.com>
+# Copyright (c) 2025-2026 Antmicro <www.antmicro.com>
 #
 # SPDX-License-Identifier: Apache-2.0
+
+RENODE_VERSION=${1:-1.16.1+20260524gitbcbf1b6ab}
 
 if [ -d renode_latest ] ; then
   echo "Renode already downloaded."
 else
   echo "Downloading Renode..."
-  curl https://builds.renode.io/renode-latest.linux-portable.tar.gz -o renode-latest.tar.gz
-  tar -xf renode-latest.tar.gz
-  rm -f renode-latest.tar.gz
-  mv -f renode_*-portable renode_latest
+  curl https://builds.renode.io/renode-${RENODE_VERSION}.linux-portable.tar.gz -o renode-pkg.tar.gz
+  tar -xf renode-pkg.tar.gz
+  rm -f renode-pkg.tar.gz
+  mv -f renode_*-portable renode_portable
 fi
 
 echo "Preparing environment..."
-export PYRENODE_BIN=$(realpath renode_latest/renode)
+export PYRENODE_BIN=$(realpath renode_portable/renode)
 export PYRENODE_RUNTIME=coreclr
-export PATH=$PATH:$(realpath renode_latest/)
+export PATH=$PATH:$(realpath renode_portable/)
